fed3b007769ba28ba8bd82796f67d554594ee878
zachabossaloler <zachabossaloler@gmail.com>
2018-08-21 16:09:48 +0100

    updated shell scripts

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-mkdir Spammy-Bot
-mv basic_bot.py Spammy-Bot
-mv README.md Spammy-Bot
-cd Spammy-Bot
-pip3 install discord
-python3 basic_bot.py
+echo starting...
+python3 spammy.py
+
---
+++ startup.sh
@@ -0,0 +1,11 @@
+touch log
+mkdir Spammy-Bot
+echo created Spammy-Bot... > log
+mv spammy.py Spammy-Bot 
+echo moving spammy.py > log
+mv README.md Spammy-Bot 
+echo moving README.md > log
+cd Spammy-Bot 
+echo changing current dir > log
+python3 spammy.py 
+echo bot is running > log
